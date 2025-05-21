4c788de6ee461498beeafaa36fb66257ff022b63
sergiosja <sergey.jakobsen@gmail.com>
2025-05-21 19:06:02 +0200

    Make it possible to print AST 🎉

--- repl/app/Main.hs
+++ repl/app/Main.hs
@@ -1,6 +1,20 @@
 module Main (main) where
 
-import Lib
+import System.Environment (getArgs)
+import Text.Parsec
+import Parser (parseProgram)
+
 
 main :: IO ()
-main = someFunc
+main = do
+    args <- getArgs
+    case args of
+        [program] -> printAST program
+        _ -> putStrLn "Scusami ma, non ho capito. Provi di nuovo, per favore."
+
+
+printAST :: String -> IO ()
+printAST program = do
+    case parse parseProgram "" program of
+        Right ast -> print ast
+        Left err -> putStrLn $ "Parse error when printing AST: " ++ show err

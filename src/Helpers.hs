db40b3c6bb32a114f01cf2d81e8dcadf36eda7e0
sergiosja <sergey.jakobsen@gmail.com>
2025-05-21 22:13:45 +0200

    Going crazy on the helpers

--- src/Helpers.hs
+++ src/Helpers.hs
@@ -1,8 +1,20 @@
-module Helpers (
-    isText,
-    isNumber,
-    isDecimal,
-    isBoolean
+module Helpers
+    ( isText
+    , isNumber
+    , isDecimal
+    , isBoolean
+    , extractText
+    , extractNumber
+    , extractDecimal
+    , extractBoolean
+    , extractTexts
+    , extractNumbers
+    , extractDecimals
+    , extractBooleans
+    , foldNumbers
+    , foldDecimals
+    , foldTexts
+    , foldBooleans
 ) where
 
 import Syntax
@@ -22,3 +34,55 @@ isDecimal _ = False
 isBoolean :: Value -> Bool
 isBoolean (Boolean _) = True
 isBoolean _ = False
+
+extractText :: Value -> String
+extractText (Text s) = s
+extractText _ = error "Called `extractText` on a non-text value"
+
+extractNumber :: Value -> Integer
+extractNumber (Number n) = n
+extractNumber _ = error "Called `extractNumber` on a non-number value"
+
+extractDecimal :: Value -> Double
+extractDecimal (Decimal d) = d
+extractDecimal _ = error "Called `extractDecimal` on a non-decimal value"
+
+extractBoolean :: Value -> Bool
+extractBoolean (Boolean b) = b
+extractBoolean _ = error "Called `extractBoolean` on a non-boolean value"
+
+extractTexts :: [Value] -> Either String [String]
+extractTexts values =
+    if all isText values
+    then Right $ map extractText values
+    else Left "Called `extractTexts` on a non-text value"
+
+extractNumbers :: [Value] -> Either String [Integer]
+extractNumbers values =
+    if all isNumber values
+    then Right $ map extractNumber values
+    else Left "Called `extractNumbers` on a non-number value"
+
+extractDecimals :: [Value] -> Either String [Double]
+extractDecimals values =
+    if all isDecimal values
+    then Right $ map extractDecimal values
+    else Left "Called `extractDecimals` on a non-decimal value"
+
+extractBooleans :: [Value] -> Either String [Bool]
+extractBooleans values =
+    if all isBoolean values
+    then Right $ map extractBoolean values
+    else Left "Called `extractBooleans` on a non-boolean value"
+
+foldNumbers :: (Integer -> Integer -> Integer) -> [Value] -> Either String Value
+foldNumbers f values = fmap (Number . foldl1 f) (extractNumbers values)
+
+foldDecimals :: (Double -> Double -> Double) -> [Value] -> Either String Value
+foldDecimals f vs = fmap (Decimal . foldl1 f) (extractDecimals vs)
+
+foldTexts :: (String -> String -> String) -> [Value] -> Either String Value
+foldTexts f vs = fmap (Text . foldl1 f) (extractTexts vs)
+
+foldBooleans :: (Bool -> Bool -> Bool) -> [Value] -> Either String Value
+foldBooleans f vs = fmap (Boolean . foldl1 f) (extractBooleans vs)

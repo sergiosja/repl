3b83c5bd6d5aaec09227f900bc295f45a51f3817
sergiosja <sergey.jakobsen@gmail.com>
2025-06-12 19:17:20 +0200

    Remove example programs

--- programs/actual-scheme.scm
+++
@@ -1,7 +0,0 @@
-(define (factorial n) (if (= n 0) 1 (* n (factorial (- n 1)))))
-
-(define (fib n) (cond ((= n 0) 0) ((= n 1) 1) (else (+ (fib (- n 1)) (fib (- n 2))))))
-
-(define (list-length lst) (if (null? lst) 0 (+ 1 (list-length (cdr lst)))))
-
-(define (square x) (* x x))
